214ebe42bc059cc533f006a7cf9beaba2a17e6d4
Rick Staa <rick.staa@outlook.com>
2024-12-29 15:10:17 +0100

    ci: cleanup Org contributor info script and change output location
    This commit cleans up the organization contributors info retrieval
    script and changes the action output location to gh-pages.

--- scripts/get_org_contributors_info.py
+++ scripts/get_org_contributors_info.py
@@ -1,14 +1,16 @@
 """Retrieves public information about organization contributors.
 
 Note:
-    Used instead of https://github.com/github/contributors to get more customized information.
+    Used instead of https://github.com/github/contributors to get more customized
+    information.
 """
 
+import datetime
 import json
 import os
 from typing import Dict, List
 
-from github import Github, PaginatedList
+from github import Github
 
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 ORG_NAME = os.getenv("ORG_NAME")
@@ -25,14 +27,17 @@ class ContributorInfo:
     """A class to represent information about a GitHub contributor.
 
     Attributes:
-        login (str): The GitHub username of the contributor.
-        name (str): The name of the contributor.
-        avatar_url (str): The URL of the contributor's avatar.
-        location (str): The location of the contributor.
-        company (str): The company of the contributor.
-        org_member (bool): Whether the contributor is a member of the organization.
-        source_contributions (int): The number of contributions to source repositories.
-        fork_contributions (int): The number of contributions to forked repositories
+        login: The GitHub username of the contributor.
+        name: The name of the contributor.
+        avatar_url: The URL of the contributor's avatar.
+        location: The location of the contributor.
+        company: The company of the contributor.
+        bio: The bio of the contributor.
+        blog_url: The URL of the contributor's blog.
+        twitter_username: The Twitter username of the contributor.
+        org_member: Whether the contributor is a member of the organization.
+        contributions: The number of contributions to source repositories.
+        yearly_contributions: The number of contributions in the last year.
     """
 
     def __init__(
@@ -42,6 +47,9 @@ class ContributorInfo:
         avatar_url: str,
         location: str,
         company: str,
+        bio: str,
+        blog_url: str,
+        twitter_username: str,
         org_member: bool,
     ):
         self.login = login
@@ -49,22 +57,28 @@ class ContributorInfo:
         self.avatar_url = avatar_url
         self.location = location
         self.company = company
+        self.bio = bio
+        self.blog_url = blog_url
+        self.twitter_username = twitter_username
         self.org_member = org_member
-        self.source_contributions = 0
-        self.fork_contributions = 0
+        self.contributions = 0
+        self.yearly_contributions = 0
 
-    def add_contributions(self, contributions: int, is_fork: bool = False):
+    def add_contributions(self, contributions: int):
         """Adds contributions to the contributor.
 
         Args:
-            contributions (int): The number of contributions to add.
-            is_fork (bool, optional): Whether the contributions are to a forked
-                repository. Defaults to False.
+            contributions: The number of contributions to add.
         """
-        if is_fork:
-            self.fork_contributions += contributions
-        else:
-            self.source_contributions += contributions
+        self.contributions += contributions
+
+    def add_yearly_contributions(self, contributions: int):
+        """Adds yearly contributions to the contributor.
+
+        Args:
+            contributions: The number of contributions to add.
+        """
+        self.yearly_contributions += contributions
 
     def to_dict(self) -> Dict[str, any]:
         """Returns a dictionary representation of the object."""
@@ -74,9 +88,12 @@ class ContributorInfo:
             "avatar_url": self.avatar_url,
             "location": self.location,
             "company": self.company,
+            "bio": self.bio,
+            "blog_url": self.blog_url,
+            "twitter_username": self.twitter_username,
             "org_member": self.org_member,
-            "source_contributions": self.source_contributions,
-            "fork_contributions": self.fork_contributions,
+            "contributions": self.contributions,
+            "yearly_contributions": self.yearly_contributions,
         }
 
 
@@ -87,6 +104,7 @@ def save_to_json(data: Dict[str, any], filename: str):
         data: The data to save.
         filename: The name of the file to save the data to.
     """
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
     with open(filename, "w") as f:
         json.dump(data, f, indent=2)
 
@@ -95,7 +113,7 @@ def get_public_org_members(org_name: str) -> List[str]:
     """Retrieves the public members of an organization.
 
     Args:
-        org_name (str): The name of the organization.
+        org_name: The name of the organization.
 
     Returns:
         A list of the organization's public members.
@@ -105,22 +123,41 @@ def get_public_org_members(org_name: str) -> List[str]:
     return [member.login for member in members]
 
 
-def get_org_repos(org_name: str, type="public") -> PaginatedList:
-    """Retrieves the repositories of an organization.
+def get_public_source_repos(org_name: str) -> List:
+    """Retrieves the public repositories of an organization, excluding forks.
 
     Args:
-        org_name (str): The name of the organization.
-        type (str, optional): The type of repositories to retrieve. Defaults to "public".
+        org_name: The name of the organization.
 
     Returns:
-        A list of the organization's repositories.
+        A list of the organization's public repositories that are not forks.
     """
     org = github_client.get_organization(org_name)
-    repos = org.get_repos(type=type)
-    return repos
+    repos = org.get_repos(type="public")
+    return [repo for repo in repos if not repo.fork]
 
 
-def get_org_contributors_info(org_name: str) -> List[Dict[str, ContributorInfo]]:
+def get_contributions_between_dates(
+    repo: str, contributor_login: str, start_date: datetime, end_date: datetime
+) -> int:
+    """Retrieves the number of contributions for a contributor between the given start and end dates.
+
+    Args:
+        repo: The repository object.
+        contributor_login: The login of the contributor.
+        start_date: The start date for the date range.
+        end_date: The end date for the date range.
+
+    Returns:
+        The number of contributions between the given dates.
+    """
+    commits = repo.get_commits(
+        author=contributor_login, since=start_date, until=end_date
+    )
+    return commits.totalCount
+
+
+def get_org_contributors_info(org_name: str) -> List[Dict[str, any]]:
     """Retrieves information about organization contributors.
 
     Args:
@@ -129,12 +166,17 @@ def get_org_contributors_info(org_name: str) -> List[Dict[str, ContributorInfo]]
     Returns:
         A list of dictionaries containing contributor information.
     """
+    print("Retrieving public organization members...")
     public_members = get_public_org_members(org_name)
-    public_repos = get_org_repos(org_name)
+    print("Retrieving public repositories...")
+    public_repos = get_public_source_repos(org_name)
 
     # Get contributors info.
+    end_date = datetime.datetime.now()
+    start_date = end_date - datetime.timedelta(days=365)
     contributors_info: Dict[str, ContributorInfo] = {}
-    for repo in public_repos:
+    print("Retrieving contributors info...")
+    for repo in public_repos[:1]:
         for contributor in repo.get_contributors():
             if contributor.login not in contributors_info:
                 contributors_info[contributor.login] = ContributorInfo(
@@ -143,12 +185,22 @@ def get_org_contributors_info(org_name: str) -> List[Dict[str, ContributorInfo]]
                     avatar_url=contributor.avatar_url,
                     location=contributor.location,
                     company=contributor.company,
+                    bio=contributor.bio,
+                    blog_url=contributor.blog,
+                    twitter_username=contributor.twitter_username,
                     org_member=contributor.login in public_members,
                 )
 
             contributors_info[contributor.login].add_contributions(
-                contributions=contributor.contributions, is_fork=repo.fork
+                contributions=contributor.contributions
             )
+            yearly_contributions = get_contributions_between_dates(
+                repo, contributor.login, start_date, end_date
+            )
+            contributors_info[contributor.login].add_yearly_contributions(
+                yearly_contributions
+            )
+        print(f"Retrieved contributors info for: {repo.full_name}")
 
     return [contributor.to_dict() for contributor in contributors_info.values()]
 
@@ -157,7 +209,9 @@ if __name__ == "__main__":
     print(f"Retrieving contributors info for {ORG_NAME}...")
     contributors_info = get_org_contributors_info(ORG_NAME)
 
-    save_to_json(contributors_info, "contributors_info.json")
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(script_dir, "data/contributors_info.json")
+    save_to_json(contributors_info, file_path)
     print(
         f"Retrieved contributions for {len(contributors_info)} contributors and saved to contributors_info.json"
     )
